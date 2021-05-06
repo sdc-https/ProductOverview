3e73dba669320ccfc00bdfc6191c0f54165c17c6
wapu-lab <nieruoxi@hotmail.com>
2021-05-05 21:13:30 -0500

    Add 3 API calls for other sellers, price and inventory

--- server/index.js
+++ server/index.js
@@ -2,69 +2,72 @@ const express = require('express');
 const app = express();
 const db = require('../database/index.js');
 const Promise = require('bluebird');
-
-const port = 3000;
-app.listen(() => {
-  console.log(`Listening to port ${port}`);
-})
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 app.get('/', (req, res) => {
   res.send('hello');
-})
+});
 
-app.get('/overview-api/otherseller/:productid', (req, res) => {
-  Promise.resolve(req.params.product_id)
+let urlAPISeller = '/overview-api/otherseller/:productid';
+let urlAPIPrice = '/overview-api/price/:productid';
+let urlAPIInventory = '/overview-api/inventory/:productid';
+
+app.get(urlAPISeller, (req, res, next) => {
+  Promise.resolve(req.params.productid)
     .then(id => {
       if (!id) {
         throw id;
       }
-      return db.getRecord(id);
+      let target = id.slice(1, id.length - 1)
+      return db.getRecord(target);
     })
     .then(records => {
-      if (!records) {
-        throw records;
-      }
       res.send(records[0].other_sellers);
+      next();
     })
     .catch(error => {
       res.send('An error has occurred');
     })
 })
 
-app.get('/overview-api/price/:productid', (req, res) => {
-  Promise.resolve(req.params.product_id)
+app.get(urlAPIPrice, (req, res, next) => {
+  Promise.resolve(req.params.productid)
     .then(id => {
       if (!id) {
         throw id;
       }
-      return db.getRecord(id);
+      let target = id.slice(1, id.length - 1)
+      return db.getRecord(target);
     })
     .then(records => {
-      if (!records) {
-        throw records;
-      }
       res.send(records[0].price);
+      next();
     })
     .catch(error => {
       res.send('An error has occurred');
     })
 })
 
-app.get('/overview-api/inventory/:productid', (req, res) => {
-  Promise.resolve(req.params.product_id)
+app.get(urlAPIInventory, (req, res, next) => {
+  Promise.resolve(req.params.productid)
     .then(id => {
       if (!id) {
         throw id;
       }
-      return db.getRecord(id);
+      let target = id.slice(1, id.length - 1)
+      return db.getRecord(target);
     })
     .then(records => {
-      if (!records) {
-        throw records;
-      }
       res.send(records[0].inventory);
+      next();
     })
     .catch(error => {
       res.send('An error has occurred');
     })
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 })
