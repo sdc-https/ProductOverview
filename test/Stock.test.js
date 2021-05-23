59640aa51bd3e87f9d30b722cd6b81649746f781
wapu-lab <nieruoxi@hotmail.com>
2021-05-23 17:37:28 -0500

    Added one more test for SellerTable

--- test/Stock.test.js
+++ test/Stock.test.js
@@ -5,6 +5,5 @@ import Stock from '../client/src/components/Stock/Stock';
 it ('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Stock />, div);
-  console.log('true');
   ReactDOM.unmountComponentAtNode(div);
 })
